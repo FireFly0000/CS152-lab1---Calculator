f3635fa742980d6d8018c631cf8e098ee848cd33
FireFly0000 <wktfirefly283@gmail.com>
2022-03-30 13:43:00 -0700

    adding task 1 and task 2

--- lex.yy.c
+++ lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
+#define YY_NUM_RULES 12
+#define YY_END_OF_BUFFER 13
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,20 +360,21 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[9] =
+static const flex_int16_t yy_accept[18] =
     {   0,
-        0,    0,    4,    3,    2,    1,    2,    0
+        0,    0,   13,   11,    9,   10,    6,    7,    5,    2,
+        3,    4,    8,    1,    9,    8,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    2,    2,    2,
-        2,    2,    2,    2,    2,    2,    2,    1,    1,    1,
-        3,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    4,
+        5,    6,    7,    1,    8,    1,    9,   10,   10,   10,
+       10,   10,   10,   10,   10,   10,   10,    1,    1,    1,
+       11,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,29 +398,36 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[4] =
+static const YY_CHAR yy_meta[12] =
     {   0,
-        1,    2,    1
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1
     } ;
 
-static const flex_int16_t yy_base[10] =
+static const flex_int16_t yy_base[18] =
     {   0,
-        0,    0,    5,    6,    0,    6,    0,    6,    2
+        0,    0,   16,   17,   13,   17,   17,   17,   17,   17,
+       17,   17,    4,   17,   11,    2,   17
     } ;
 
-static const flex_int16_t yy_def[10] =
+static const flex_int16_t yy_def[18] =
     {   0,
-        8,    1,    8,    8,    9,    8,    9,    0,    8
+       17,    1,   17,   17,   17,   17,   17,   17,   17,   17,
+       17,   17,   17,   17,   17,   17,    0
     } ;
 
-static const flex_int16_t yy_nxt[10] =
+static const flex_int16_t yy_nxt[29] =
     {   0,
-        4,    5,    6,    7,    8,    3,    8,    8,    8
+        4,    5,    6,    7,    8,    9,   10,   11,   12,   13,
+       14,   16,   15,   16,   15,   17,    3,   17,   17,   17,
+       17,   17,   17,   17,   17,   17,   17,   17
     } ;
 
-static const flex_int16_t yy_chk[10] =
+static const flex_int16_t yy_chk[29] =
     {   0,
-        1,    1,    1,    9,    3,    8,    8,    8,    8
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,   16,   15,   13,    5,    3,   17,   17,   17,   17,
+       17,   17,   17,   17,   17,   17,   17,   17
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -436,14 +444,12 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "calc.lex"
-#line 2 "calc.lex"
+#line 1 "calc_task2.lex"
+#line 2 "calc_task2.lex"
    /* cs152-calculator */
-   /* write your C code here for defination of variables and including headers */
-#line 444 "lex.yy.c"
-#line 9 "calc.lex"
-   /* some common rules, for example DIGIT */
-#line 447 "lex.yy.c"
+   int currLine = 1, currPos = 1;
+#line 452 "lex.yy.c"
+#line 453 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -660,11 +666,9 @@ YY_DECL
 		}
 
 	{
-#line 12 "calc.lex"
+#line 9 "calc_task2.lex"
 
-   /* specific lexer rules in regex */
-
-#line 668 "lex.yy.c"
+#line 672 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -691,13 +695,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 9 )
+				if ( yy_current_state >= 18 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 17 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -723,20 +727,66 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 15 "calc.lex"
-{printf("EQUAL\n"); }
+#line 10 "calc_task2.lex"
+{printf("EQUAL\n"); currPos += yyleng;}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 16 "calc.lex"
-{printf("NUMBER %s\n", yytext); }
+#line 11 "calc_task2.lex"
+{printf("PLUS\n"); currPos += yyleng;}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 18 "calc.lex"
+#line 12 "calc_task2.lex"
+{printf("MINUS\n"); currPos += yyleng;}
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 13 "calc_task2.lex"
+{printf("DIV\n"); currPos += yyleng;}
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 14 "calc_task2.lex"
+{printf("MULT\n"); currPos += yyleng;}
+	YY_BREAK
+case 6:
+YY_RULE_SETUP
+#line 15 "calc_task2.lex"
+{printf("L_PAREN\n"); currPos += yyleng;}
+	YY_BREAK
+case 7:
+YY_RULE_SETUP
+#line 16 "calc_task2.lex"
+{printf("R_PAREN\n"); currPos += yyleng;}
+	YY_BREAK
+case 8:
+YY_RULE_SETUP
+#line 17 "calc_task2.lex"
+{printf("NUMBER %s\n", yytext); currPos += yyleng;}
+	YY_BREAK
+case 9:
+YY_RULE_SETUP
+#line 19 "calc_task2.lex"
+{/* ignore spaces */ currPos += yyleng;}
+	YY_BREAK
+case 10:
+/* rule 10 can match eol */
+YY_RULE_SETUP
+#line 21 "calc_task2.lex"
+{currLine++; currPos = 1;}
+	YY_BREAK
+case 11:
+YY_RULE_SETUP
+#line 23 "calc_task2.lex"
+{printf("Error at line %d, column %d: unrecognized symbol \"%s\" \n", currLine, currPos, yytext); exit(0);}
+	YY_BREAK
+case 12:
+YY_RULE_SETUP
+#line 25 "calc_task2.lex"
 ECHO;
 	YY_BREAK
-#line 740 "lex.yy.c"
+#line 790 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1033,7 +1083,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 9 )
+			if ( yy_current_state >= 18 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1061,11 +1111,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 9 )
+		if ( yy_current_state >= 18 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 8);
+	yy_is_jam = (yy_current_state == 17);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1741,13 +1791,21 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 18 "calc.lex"
+#line 25 "calc_task2.lex"
 
-	/* C functions used in lexer */
 
 int main(int argc, char ** argv)
 {
-   yylex();
+   	if(argc >= 2){
+		yyin = fopen(argv[1], "r");
+		if(yyin == NULL){
+			yyin = stdin;
+		}	
+	}
+	else{
+		yyin = stdin;
+	}
+	yylex();
 }
 
 
